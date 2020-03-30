2f9fe821de1d0e0536f32ddc7a9ce618aaf7afd9
Andrés Báez <u1803627@unimilitar.edu.co>
2020-03-29 22:30:46 -0500

    Cambios

--- ControlTemperatura/src/controltemperatura/ControlTemperatura.java
+++ ControlTemperatura/src/controltemperatura/ControlTemperatura.java
@@ -5,17 +5,52 @@
  */
 package controltemperatura;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author andresbaezrincon
  */
 public class ControlTemperatura {
+        
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entradateclado = new Scanner(System.in);
+        ArrayList<Persona> listaPersona = new ArrayList<Persona>();
+        String seguir,nombre,parentesco;
+        int opc,edad;
+        float temperatura;
+        seguir = "S";
+        while(seguir == "S" || seguir == "s"){
+            System.out.println("*********************************");
+            System.out.println("*                               *");
+            System.out.println("*  1. Ingresar Nueva Persona    *");
+            System.out.println("*  2. Ver lista de Personas     *");            
+            System.out.println("*  2. Ingresar Temperatura      *");
+            System.out.println("*  3. Eliminar Persona          *");
+            System.out.println("*  4. Generar Reporte           *");
+            System.out.println("*                               *");
+            System.out.println("*********************************");
+            System.out.print("Ingrese la opcion a realizar: ");
+            opc = Integer.parseInt(entradateclado.nextLine());
+            switch (opc){
+                case 1:
+                    listaPersona.add(new Persona());
+                    System.out.println("Persona creada correctamente.");
+                break;
+                case 2:
+                 System.out.println("Nombre:");
+                 for(int i=0;i<listaPersona.size();i++)
+                     System.out.println((i+1)+"."+listaPersona.get(i).Nombre);
+                 
+                break;
+            }
+        
+        }
     }
     
 }
---
+++ ControlTemperatura/src/controltemperatura/Persona.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controltemperatura;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author andresbaezrincon
+ */
+public class Persona {
+    Scanner entradaTeclado = new Scanner(System.in);
+    String Nombre;
+    int Edad;
+    String Parentesco;
+    ArrayList<Persona> listaTemperatura= new ArrayList<Persona>();
+    
+    Persona(){
+        
+        this.Nombre = ingresoNombre();
+        this.Edad = ingresoEdad();
+        this.Parentesco = ingresoParentesco();
+    }
+    String ingresoNombre(){
+        String Nombre;
+        System.out.print("Ingrese el nombre de la persona: ");
+        Nombre = entradaTeclado.nextLine();  
+        return Nombre;
+    }
+    int ingresoEdad(){
+        int Edad;
+        System.out.print("Ingrese la edad de la persona: ");
+        Edad = Integer.parseInt(entradaTeclado.nextLine());  
+        return Edad;
+    }
+    String ingresoParentesco(){
+        String Parentesco;
+        System.out.print("Ingrese el parentesco de la persona: ");
+        Parentesco = entradaTeclado.nextLine();  
+        return Parentesco;
+    }
+}
